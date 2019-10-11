8a443f23acdf0ed1120aa0d482a93a58f4f664be
Akash Bathla <akash.bathla2013@gmail.com>
2019-10-11 16:34:31 +0530

    resideo changes

--- js/mapee61.js
+++ js/mapee61.js
@@ -1,4 +1,4 @@
-(function($) {
+(function ($) {
     "use strict";
 
     var map;
@@ -7,8 +7,8 @@
     var styles;
     var propertiesList;
     var options = {
-        zoom : 14,
-        mapTypeId : 'Styled',
+        zoom: 14,
+        mapTypeId: 'Styled',
         panControl: false,
         zoomControl: true,
         mapTypeControl: false,
@@ -22,7 +22,7 @@
         fullscreenControl: false,
     };
 
-    styles = [{"featureType": "water","elementType": "geometry","stylers": [{"color": "#e9e9e9"},{"lightness": 17}]},{"featureType": "landscape","elementType": "geometry","stylers": [{"color": "#f5f5f5"},{"lightness": 20}]},{"featureType": "road.highway","elementType": "geometry.fill","stylers": [{"color": "#ffffff"},{"lightness": 17}]},{"featureType": "road.highway","elementType": "geometry.stroke","stylers": [{"color": "#ffffff"},{"lightness": 29},{"weight": 0.2}]},{"featureType": "road.arterial","elementType": "geometry","stylers": [{"color": "#ffffff"},{"lightness": 18}]},{"featureType": "road.local","elementType": "geometry","stylers": [{"color": "#ffffff"},{"lightness": 16}]},{"featureType": "poi","elementType": "geometry","stylers": [{"color": "#f5f5f5"},{"lightness": 21}]},{"featureType": "poi.park","elementType": "geometry","stylers": [{"color": "#dedede"},{"lightness": 21}]},{"elementType": "labels.text.stroke","stylers": [{"visibility": "on"},{"color": "#ffffff"},{"lightness": 16}]},{"elementType": "labels.text.fill","stylers": [{"saturation": 36},{"color": "#333333"},{"lightness": 40}]},{"elementType": "labels.icon","stylers": [{"visibility": "off"}]},{"featureType": "transit","elementType": "geometry","stylers": [{"color": "#f2f2f2"},{"lightness": 19}]},{"featureType": "administrative","elementType": "geometry.fill","stylers": [{"color": "#fefefe"},{"lightness": 20}]},{"featureType": "administrative","elementType": "geometry.stroke","stylers": [{"color": "#fefefe"},{"lightness": 17},{"weight": 1.2}]}];
+    styles = [{ "featureType": "water", "elementType": "geometry", "stylers": [{ "color": "#e9e9e9" }, { "lightness": 17 }] }, { "featureType": "landscape", "elementType": "geometry", "stylers": [{ "color": "#f5f5f5" }, { "lightness": 20 }] }, { "featureType": "road.highway", "elementType": "geometry.fill", "stylers": [{ "color": "#ffffff" }, { "lightness": 17 }] }, { "featureType": "road.highway", "elementType": "geometry.stroke", "stylers": [{ "color": "#ffffff" }, { "lightness": 29 }, { "weight": 0.2 }] }, { "featureType": "road.arterial", "elementType": "geometry", "stylers": [{ "color": "#ffffff" }, { "lightness": 18 }] }, { "featureType": "road.local", "elementType": "geometry", "stylers": [{ "color": "#ffffff" }, { "lightness": 16 }] }, { "featureType": "poi", "elementType": "geometry", "stylers": [{ "color": "#f5f5f5" }, { "lightness": 21 }] }, { "featureType": "poi.park", "elementType": "geometry", "stylers": [{ "color": "#dedede" }, { "lightness": 21 }] }, { "elementType": "labels.text.stroke", "stylers": [{ "visibility": "on" }, { "color": "#ffffff" }, { "lightness": 16 }] }, { "elementType": "labels.text.fill", "stylers": [{ "saturation": 36 }, { "color": "#333333" }, { "lightness": 40 }] }, { "elementType": "labels.icon", "stylers": [{ "visibility": "off" }] }, { "featureType": "transit", "elementType": "geometry", "stylers": [{ "color": "#f2f2f2" }, { "lightness": 19 }] }, { "featureType": "administrative", "elementType": "geometry.fill", "stylers": [{ "color": "#fefefe" }, { "lightness": 20 }] }, { "featureType": "administrative", "elementType": "geometry.stroke", "stylers": [{ "color": "#fefefe" }, { "lightness": 17 }, { "weight": 1.2 }] }];
 
     propertiesList = [{
         id: 1,
@@ -80,7 +80,7 @@
         }
     }, {
         id: 4,
-        title: 'Beautiful House in Marina',
+        title: 'Beautiful House in Hydrabad',
         photo: 'images/properties/prop-7-1-thmb.jpg',
         position: {
             lat: '37.748134',
@@ -135,17 +135,17 @@
     }];
 
     function CustomMarker(id, latlng, map, classname, html) {
-        this.id        = id;
-        this.latlng_   = latlng;
+        this.id = id;
+        this.latlng_ = latlng;
         this.classname = classname;
-        this.html      = html;
+        this.html = html;
 
         this.setMap(map);
     }
 
     CustomMarker.prototype = new google.maps.OverlayView();
 
-    CustomMarker.prototype.draw = function() {
+    CustomMarker.prototype.draw = function () {
         var me = this;
         var div = this.div_;
 
@@ -154,7 +154,7 @@
             div.classList.add(this.classname);
             div.innerHTML = this.html;
 
-            google.maps.event.addDomListener(div, 'click', function(event) {
+            google.maps.event.addDomListener(div, 'click', function (event) {
                 google.maps.event.trigger(me, 'click');
             });
 
@@ -170,43 +170,43 @@
         }
     };
 
-    CustomMarker.prototype.remove = function() {
+    CustomMarker.prototype.remove = function () {
         if (this.div_) {
             this.div_.parentNode.removeChild(this.div_);
             this.div_ = null;
         }
     };
 
-    CustomMarker.prototype.getPosition = function() {
+    CustomMarker.prototype.getPosition = function () {
         return this.latlng_;
     };
 
-    CustomMarker.prototype.addActive = function() {
+    CustomMarker.prototype.addActive = function () {
         if (this.div_) {
             $('.pxp-price-marker').removeClass('active');
             this.div_.classList.add('active');
         }
     };
 
-    CustomMarker.prototype.removeActive = function() {
+    CustomMarker.prototype.removeActive = function () {
         if (this.div_) {
             this.div_.classList.remove('active');
         }
     };
 
     function addMarkers(props, map) {
-        $.each(props, function(i, prop) {
+        $.each(props, function (i, prop) {
             var latlng = new google.maps.LatLng(prop.position.lat, prop.position.lng);
 
-            var html = '<div class="pxp-marker-short-price">' + prop.price.short + '</div>' + 
-                        '<a href="' + prop.link + '" class="pxp-marker-details">' + 
-                            '<div class="pxp-marker-details-fig pxp-cover" style="background-image: url(' + prop.photo + ');"></div>' + 
-                            '<div class="pxp-marker-details-info">' + 
-                                '<div class="pxp-marker-details-info-title">' + prop.title + '</div>' + 
-                                '<div class="pxp-marker-details-info-price">' + prop.price.long + '</div>' + 
-                                '<div class="pxp-marker-details-info-feat">' + prop.features.beds + ' BD<span>|</span>' + prop.features.baths + ' BA<span>|</span>' + prop.features.size + '</div>' + 
-                            '</div>' + 
-                        '</a>';
+            var html = '<div class="pxp-marker-short-price">' + prop.price.short + '</div>' +
+                '<a href="' + prop.link + '" class="pxp-marker-details">' +
+                '<div class="pxp-marker-details-fig pxp-cover" style="background-image: url(' + prop.photo + ');"></div>' +
+                '<div class="pxp-marker-details-info">' +
+                '<div class="pxp-marker-details-info-title">' + prop.title + '</div>' +
+                '<div class="pxp-marker-details-info-price">' + prop.price.long + '</div>' +
+                '<div class="pxp-marker-details-info-feat">' + prop.features.beds + ' BD<span>|</span>' + prop.features.baths + ' BA<span>|</span>' + prop.features.size + '</div>' +
+                '</div>' +
+                '</a>';
 
             var marker = new CustomMarker(prop.id, latlng, map, 'pxp-price-marker', html);
 
@@ -215,20 +215,20 @@
         });
     }
 
-    setTimeout(function() {
-        if($('#results-map').length > 0) {
+    setTimeout(function () {
+        if ($('#results-map').length > 0) {
             map = new google.maps.Map(document.getElementById('results-map'), options);
             var styledMapType = new google.maps.StyledMapType(styles, {
-                name : 'Styled',
+                name: 'Styled',
             });
 
             map.mapTypes.set('Styled', styledMapType);
-            map.setCenter(new google.maps.LatLng(37.7577627,-122.4726194));
+            map.setCenter(new google.maps.LatLng(37.7577627, -122.4726194));
             map.setZoom(15);
 
             addMarkers(propertiesList, map);
 
-            map.fitBounds(markers.reduce(function(bounds, marker) {
+            map.fitBounds(markers.reduce(function (bounds, marker) {
                 return bounds.extend(marker.getPosition());
             }, new google.maps.LatLngBounds()));
 
@@ -253,27 +253,27 @@
 
             google.maps.event.trigger(map, 'resize');
 
-            $('.pxp-results-card-1').each(function(i) {
+            $('.pxp-results-card-1').each(function (i) {
                 var propID = $(this).attr('data-prop');
 
-                $(this).on('mouseenter', function() {
+                $(this).on('mouseenter', function () {
                     if (map) {
-                        var targetMarker = $.grep(markers, function(e) {
+                        var targetMarker = $.grep(markers, function (e) {
                             return e.id == propID;
                         });
 
-                        if(targetMarker.length > 0) {
+                        if (targetMarker.length > 0) {
                             targetMarker[0].addActive();
                             map.setCenter(targetMarker[0].latlng_);
                         }
                     }
                 });
-                $(this).on('mouseleave', function() {
-                    var targetMarker = $.grep(markers, function(e) {
+                $(this).on('mouseleave', function () {
+                    var targetMarker = $.grep(markers, function (e) {
                         return e.id == propID;
                     });
 
-                    if(targetMarker.length > 0) {
+                    if (targetMarker.length > 0) {
                         targetMarker[0].removeActive();
                     }
                 });
